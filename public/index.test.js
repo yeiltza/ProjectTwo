6a742a62acd7c2abd974bae93cbc893d2fa20558
Julaam Diop <jeebe.diop@gmail.com>
2021-10-20 20:10:21 -0500

    Add two test cases using testing library on DOM

--- public/index.test.js
+++ public/index.test.js
@@ -1,7 +1,7 @@
-const {screen, getByText, fireEvent} = require('@testing-library/dom')
+const {screen, getByText, fireEvent} = require('@testing-library/dom');
 const { JSDOM } = require('jsdom');
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 require('@testing-library/jest-dom/extend-expect');
 
 const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
@@ -10,15 +10,21 @@ let dom;
 let container;
 
 describe('index html test', () => {
-    beforeEach(() => {
-        dom = new JSDOM(html, {runScripts: 'dangerously'})
-        container = dom.window.document.body
-    })
+  beforeEach(() => {
+    dom = new JSDOM(html, {runScripts: 'dangerously'});
+    container = dom.window.document.body;
+  });
 
-    it('should render an h1 that says welcome', () => {
-        const h1 = container.querySelector('h1')
-        expect(h1).not.toBeNull()
-        expect(getByText(container, 'Express')).toBeInTheDocument()
-    })
-})
+  it('should render an h1 that says Express', () => {
+    const h1 = container.querySelector('h1');
+    expect(h1).not.toBeNull();
+    expect(getByText(container, 'Express')).toBeInTheDocument();
+  });
+
+  it('should render a p that says Welcome to Express', () => {
+    const p = container.querySelector('p');
+    expect(p).not.toBeNull();
+    expect(getByText(container, 'Welcome to Express')).toBeInTheDocument();
+  });
+});
 
