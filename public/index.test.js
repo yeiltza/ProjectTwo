f27e557cab095c8bad1d4ebf4e241f3c38c0400e
Julaam Diop <jeebe.diop@gmail.com>
2021-10-20 19:28:58 -0500

    Add testing and linting using ESLint and testing library

---
+++ public/index.test.js
@@ -0,0 +1,24 @@
+const {screen, getByText, fireEvent} = require('@testing-library/dom')
+const { JSDOM } = require('jsdom');
+const fs = require('fs')
+const path = require('path')
+require('@testing-library/jest-dom/extend-expect');
+
+const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+
+let dom;
+let container;
+
+describe('index html test', () => {
+    beforeEach(() => {
+        dom = new JSDOM(html, {runScripts: 'dangerously'})
+        container = dom.window.document.body
+    })
+
+    it('should render an h1 that says welcome', () => {
+        const h1 = container.querySelector('h1')
+        expect(h1).not.toBeNull()
+        expect(getByText(container, 'Express')).toBeInTheDocument()
+    })
+})
+
