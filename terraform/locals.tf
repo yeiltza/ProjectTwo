32d91dd659c93684a02f68653e189b6ca67eb792
Julaam Diop <jeebe.diop@gmail.com>
2021-11-04 15:25:31 -0500

    Create local variables to be used for provisioning of IAM policies

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  eb_policy = jsonencode({
+  admin_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
@@ -330,26 +330,15 @@ locals {
           "arn:aws:sqs:*:*:awseb-e-*",
           "arn:aws:sqs:*:*:eb-*"
         ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:*",
+          "s3-object-lambda:*"
+        ],
+        "Resource" : "*"
       }
     ]
   })
-
-}
-
-variable "aws_region" {
-  description = "Region in which AWS resources will be created"
-  type        = string
-  default     = "us-east-2"
-}
-
-variable "solution_stack_name" {
-  description = "The language/application that the Linux AMI is running"
-  type        = string
-  default     = "64bit Amazon Linux 2018.03 v2.17.1 running Docker 20.10.7-ce"
-}
-
-variable "app_name" {
-  description = "The name of the application"
-  type        = string
-  default     = "webrtc-app"
 }
