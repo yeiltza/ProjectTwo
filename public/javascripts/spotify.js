220157131fb9cd2538c8209a83c0a88b31d823b1
Julaam Diop <jeebe.diop@gmail.com>
2021-11-04 22:43:11 -0500

    Add in spotify web playback SDK from https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#

--- public/javascripts/spotify.js
+++ public/javascripts/spotify.js
@@ -46,3 +46,43 @@ function generateRandomString(length) {
   }
   return text;
 }
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token =
+    "BQCxBJ4_ooFlawSxkui-GJvnea5TlVzJHVUl3ewoho8d_z0odSlrQG0_w9wVK_wFtUUrjPxMqzam26FD2yQoNasZDo-aRu8oTMQdQaX_FNCN4NGEpbNTSuDU7Q060_KNnz15axmgR9dsRL8XbdgxSyYkUSnPaatI-HQaDvkc4vgk";
+  const player = new Spotify.Player({
+    name: "Web Playback SDK Quick Start Player",
+    getOAuthToken: (cb) => {
+      cb(token);
+    },
+    volume: 0.5,
+  });
+
+  // Ready
+  player.addListener("ready", ({ device_id }) => {
+    console.log("Ready with Device ID", device_id);
+  });
+
+  // Not Ready
+  player.addListener("not_ready", ({ device_id }) => {
+    console.log("Device ID has gone offline", device_id);
+  });
+
+  player.addListener("initialization_error", ({ message }) => {
+    console.error(message);
+  });
+
+  player.addListener("authentication_error", ({ message }) => {
+    console.error(message);
+  });
+
+  player.addListener("account_error", ({ message }) => {
+    console.error(message);
+  });
+
+  document.getElementById("togglePlay").onclick = function () {
+    player.togglePlay();
+  };
+
+  player.connect();
+};
