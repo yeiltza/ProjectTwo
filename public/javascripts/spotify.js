634da12620a494de2dfabd0353b90e3786db3a23
Julaam Diop <jeebe.diop@gmail.com>
2021-11-04 21:03:57 -0500

    Add spotify integration from code privded at https://github.com/spotify/web-api-auth-examples/blob/master/implicit_grant/public/index.html

---
+++ public/javascripts/spotify.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const client_id = "829cb5438c7443429b292589e44dd040";
+const redirect_uri = "http://localhost:8888/callback";
+
+const stateKey = "spotify_auth_state";
+const state = generateRandomString(16);
+
+localStorage.setItem(stateKey, state);
+const scope = "user-read-private user-read-email";
+
+let url = "https://accounts.spotify.com/authorize";
+url += "?response_type=token";
+url += "&client_id=" + encodeURIComponent(client_id);
+url += "&scope=" + encodeURIComponent(scope);
+url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+url += "&state=" + encodeURIComponent(state);
+
+/**
+ * Obtains parameters from the hash of the URL
+ * @return Object
+ */
+function getHashParams() {
+  var hashParams = {};
+  var e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  while ((e = r.exec(q))) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+}
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+function generateRandomString(length) {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
