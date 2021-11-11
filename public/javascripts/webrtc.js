a82771cf6e970d191ec77a30c1e3596eeac17150
Jeebe Diop <jeebe.diop@gmail.com>
2021-11-11 16:16:39 -0600

    Revert "Adjust video element html and create function to get video stream permissions"
    This reverts commit fcf95f69e39eedc897e5d81a1338807a46b58d0f.

--- public/javascripts/webrtc.js
+++ public/javascripts/webrtc.js
@@ -1,17 +1,1 @@
 "use strict";
-
-const $self = {
-  constraints: {
-    audio: false,
-    video: true,
-  },
-};
-
-requestUserMedia($self.constraints);
-
-// On page load, asks for permission to use audio/video.
-async function requestUserMedia(constraints) {
-  const video = document.querySelector("#self");
-  $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
-  video.srcObject = $self.stream;
-}
