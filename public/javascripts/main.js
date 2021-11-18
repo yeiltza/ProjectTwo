09a9eb0fa78aa99cc5a24ec34cab6dcce1784574
Yelitza <ycastro@hawk.iit.edu>
2021-11-17 21:18:43 -0600

    Add call functions

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -21,3 +21,42 @@
  const sc = io.connect('/' + namespace, { autoConnect: false });
 
  registerScCallbacks();
+
+ /**
+  *  Begin Application-Specific Code
+  */
+ /**
+  *  User-Interface Setup
+  */
+  document.querySelector('#header h1')
+    .innerText = 'Welcome to Room #' + namespace;
+
+  document.querySelector('#call-button')
+    .addEventListener('click', handleCallButton);
+
+  document.querySelector('#username-form')
+    .addEventListener('submit', handleUsernameForm);
+
+    /**
+   *  User Features and Media Setup*/
+
+  requestUserMedia($self.mediaConstraints);
+
+  /**
+   *  User-Interface Functions and Callbacks
+   */
+
+  function handleCallButton(event) {
+    const callButton = event.target;
+    if (callButton.className === 'join') {
+      console.log('Joining the call...');
+      callButton.className = 'leave';
+      callButton.innerText = 'Leave Call';
+      joinCall();
+    } else {
+      console.log('Leaving the call...');
+      callButton.className = 'join';
+      callButton.innerText = 'Join Call';
+      leaveCall();
+    }
+  }
