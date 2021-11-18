ba6656491364e3599f60309838fcd11a077ca39a
Yelitza <ycastro@hawk.iit.edu>
2021-11-17 21:32:31 -0600

    Add app.js necessary code to be compatible with demo in week 13

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -173,3 +173,242 @@ function resetCall(id, disconnect) {
     delete $peers[id];
   }
 }
+**
+ *  WebRTC Functions and Callbacks
+ */
+
+function registerRtcCallbacks(id) {
+  const peer = $peers[id];
+  peer.connection
+    .onconnectionstatechange = handleRtcConnectionStateChange(id);
+  peer.connection
+    .onnegotiationneeded = handleRtcConnectionNegotiation(id);
+  peer.connection
+    .onicecandidate = handleRtcIceCandidate(id);
+  peer.connection
+    .ontrack = handleRtcPeerTrack(id);
+  peer.connection
+    .ondatachannel = handleRtcDataChannel(id);
+}
+
+function handleRtcPeerTrack(id) {
+  return function({ track, streams: [stream] }) {
+    console.log('Attempt to display media from peer...');
+    displayStream(`#peer-${id}`, stream);
+  }
+}
+
+function handleRtcDataChannel(id) {
+  return function({ channel }) {
+    const label = channel.label;
+    console.log(`Data channel added for ${label}`);
+    if (label.startsWith('username-')) {
+      document.querySelector(`#peer-${id} figcaption`)
+        .innerText = label.split('username-')[1];
+      channel.onopen = function() {
+        channel.close();
+      };
+    }
+  }
+}
+
+
+/**
+ * =========================================================================
+ *  End Application-Specific Code
+ * =========================================================================
+ */
+
+
+
+/**
+ *  Reusable WebRTC Functions and Callbacks
+ */
+
+function handleRtcConnectionNegotiation(id) {
+  return async function() {
+    const peer = $peers[id];
+    if ($self[id].isSuppressingInitialOffer) return;
+    try {
+      $self[id].isMakingOffer = true;
+      await peer.connection.setLocalDescription();
+    } catch(e) {
+      const offer = await peer.connection.createOffer();
+      await peer.connection.setLocalDescription(offer);
+    } finally {
+      sc.emit('signal',
+        { to: id, from: $self.id,
+          signal: { description: peer.connection.localDescription } });
+      $self[id].isMakingOffer = false;
+    }
+  }
+}
+
+function handleRtcIceCandidate(id) {
+  return function({ candidate }) {
+    console.log('MY ICE CANDIDATE', candidate);
+    sc.emit('signal', { to: id, from: $self.id,
+      signal: { candidate: candidate } });
+  }
+}
+
+function handleRtcConnectionStateChange(id) {
+  return function() {
+    const connectionState = $peers[id].connection.connectionState;
+    document.querySelector(`#peer-${id}`).className = connectionState;
+  }
+}
+
+
+
+/**
+ *  Signaling-Channel Functions and Callbacks
+ */
+
+function registerScCallbacks() {
+  sc.on('connect', handleScConnect);
+  sc.on('connected peer', handleScConnectedPeer);
+  sc.on('connected peers', handleScConnectedPeers);
+  sc.on('disconnected peer', handleScDisconnectedPeer);
+  sc.on('signal', handleScSignal);
+}
+
+function handleScConnect() {
+  console.log('Successfully connected to the signaling server!');
+  $self.id = sc.id;
+}
+
+function handleScConnectedPeer(id) {
+  console.log('Heard new connected peer ID:', id);
+  initializeSelfAndPeerById(id, false);
+  establishCallFeatures(id);
+}
+
+function handleScConnectedPeers(ids) {
+  console.log('Received already-connected peer IDs', ids.join(', '));
+  for (let id of ids) {
+    if (id !== $self.id) {
+      initializeSelfAndPeerById(id, true);
+      establishCallFeatures(id);
+    }
+  }
+}
+
+function handleScDisconnectedPeer(id) {
+  console.log('Heard new disconnected peer ID:', id);
+  resetCall(id, true);
+}
+
+function resetAndRetryConnection(id) {
+  const isPolite = $self[id].isPolite;
+  resetCall(id, false);
+  initializeSelfAndPeerById(id, isPolite);
+  $self[id].isSuppressingInitialOffer = isPolite;
+
+  establishCallFeatures(id);
+
+  if (isPolite) {
+    sc.emit('signal', { to: id, from: $self.id,
+      signal: { description: { type: '_reset' } } });
+  }
+}
+
+async function handleScSignal({ from, signal: { description, candidate } }) {
+  const id = from;
+  const peer = $peers[id];
+  if (description) {
+
+    if (description.type === '_reset') {
+      resetAndRetryConnection(id);
+      return;
+    }
+
+    const readyForOffer =
+          !$self[id].isMakingOffer &&
+          (peer.connection.signalingState === 'stable'
+            || $self[id].isSettingRemoteAnswerPending);
+
+    const offerCollision = description.type === 'offer' && !readyForOffer;
+
+    $self[id].isIgnoringOffer = !$self[id].isPolite && offerCollision;
+
+    if ($self[id].isIgnoringOffer) {
+      return;
+    }
+
+    $self[id].isSettingRemoteAnswerPending = description.type === 'answer';
+    try {
+      console.log('Signaling state on incoming description:',
+        peer.connection.signalingState);
+      await peer.connection.setRemoteDescription(description);
+    } catch(e) {
+      resetAndRetryConnection(id);
+      return;
+    }
+    $self[id].isSettingRemoteAnswerPending = false;
+
+    if (description.type === 'offer') {
+      try {
+        await peer.connection.setLocalDescription();
+      } catch(e) {
+        const answer = await peer.connection.createAnswer();
+        await peer.connection.setLocalDescription(answer);
+      } finally {
+        sc.emit('signal', { to: id, from: $self.id, signal:
+          { description: peer.connection.localDescription } });
+        $self[id].isSuppressingInitialOffer = false;
+      }
+    }
+  } else if (candidate) {
+    // Handle ICE candidates
+    try {
+      console.log(`INCOMING ICE CANDIDATE for ${id}`, candidate);
+      await peer.connection.addIceCandidate(candidate);
+    } catch(e) {
+      // Log error unless $self[id] is ignoring offers
+      // and candidate is not an empty string
+      if (!$self[id].isIgnoringOffer && candidate.candidate.length > 1) {
+        console.error('Unable to add ICE candidate for peer:', e);
+      }
+    }
+  }
+}
+
+
+
+/**
+ *  Utility Functions
+ */
+
+function prepareNamespace(hash, set_location) {
+  let ns = hash.replace(/^#/, ''); // remove # from the hash
+  if (/^[a-z]{3}-[a-z]{4}-[a-z]{3}$/.test(ns)) {
+    console.log(`Checked existing namespace '${ns}'`);
+    return ns;
+  }
+
+  ns = generateRandomAlphaString('-', 3, 4, 3);
+
+  console.log(`Created new namespace '${ns}'`);
+  if (set_location) window.location.hash = ns;
+  return ns;
+}
+
+function generateRandomAlphaString(separator, ...groups) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let ns = [];
+  for (let group of groups) {
+    let str = '';
+    for (let i = 0; i < group; i++) {
+      str += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    ns.push(str);
+  }
+  return ns.join(separator);
+}
+
+function resetObjectKeys(obj) {
+  for (let key of Object.keys(obj)) {
+    delete obj[key];
+  }
+}
