45ea4fe78b4efd3ec7ec6e3fdf87de68417a9ea7
Julaam Diop <jeebe.diop@gmail.com>
2021-11-18 13:21:29 -0600

    Delete copy-pasted webrtc code and move socketio to webrtc.js file

--- public/javascripts/webrtc.js
+++ public/javascripts/webrtc.js
@@ -15,3 +15,34 @@ async function requestUserMedia(constraints) {
   $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
   video.srcObject = $self.stream;
 }
+
+const namespace = prepareNamespace(window.location.hash, true);
+
+const sc = io.connect("/" + namespace, { autoConnect: false });
+
+function prepareNamespace(hash, set_location) {
+  let ns = hash.replace(/^#/, ""); // remove # from the hash
+  if (/^[a-z]{3}-[a-z]{4}-[a-z]{3}$/.test(ns)) {
+    console.log(`Checked existing namespace '${ns}'`);
+    return ns;
+  }
+
+  ns = generateRandomAlphaString("-", 3, 4, 3);
+
+  console.log(`Created new namespace '${ns}'`);
+  if (set_location) window.location.hash = ns;
+  return ns;
+}
+
+function generateRandomAlphaString(separator, ...groups) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let ns = [];
+  for (let group of groups) {
+    let str = "";
+    for (let i = 0; i < group; i++) {
+      str += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    ns.push(str);
+  }
+  return ns.join(separator);
+}
