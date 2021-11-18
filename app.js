2bf8bd2cc7f2cc9e38042501775d07cac16039ac
Jeebe Diop <jeebe.diop@gmail.com>
2021-11-18 05:15:23 -0600

    Finish styling main pages (#15)
    * Merge new changes
    
    * Add styling for topics page and navbar
    
    * Remove default button styles and add to reset
    
    * Finish adding home styles and add SVG to index page
    
    * Link sass stylesheet to topics page and add sass skeleton
    
    * Add stylings for topics page and add wrappers for topic elements
    
    * Fix typo, add default button styles, and adjust spotify page html
    
    * Format code with prettier and make logo route to home
    
    * Format index.html with prettier and fix closing /a tag
    
    * Add some wonky js in an attempt to remove .html file endings
    
    * Delete wonky js and use express to route /pages without .html, and format app.js with prettier

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const io = require('socket.io')();
+const io = require("socket.io")();
 
 //Add conts variable for Spotify router
 const spotifyRouter = require("./routes/spotify.router");
@@ -13,15 +13,19 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    extensions: ["html", "htm"],
+  })
+);
 
 app.use("/spotify", spotifyRouter);
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
-const namespaces = io.of(/^\/[a-z]{3}\-[a-z]{4}\-[a-z]{3}$/)
+const namespaces = io.of(/^\/[a-z]{3}\-[a-z]{4}\-[a-z]{3}$/);
 
-namespaces.on('connection', function(socket) {
+namespaces.on("connection", function (socket) {
   const namespace = socket.nsp;
   const peers = [];
 
@@ -30,21 +34,20 @@ namespaces.on('connection', function(socket) {
   }
 
   // Send array of all peer IDs to new connector
-  socket.emit('connected peers', peers);
+  socket.emit("connected peers", peers);
 
   // Send new connector peer ID to all connected peers
-  socket.broadcast.emit('connected peer', socket.id);
+  socket.broadcast.emit("connected peer", socket.id);
 
   // listen for and route signals
-  socket.on('signal', function({ to, from, signal }) {
-    socket.to(to).emit('signal', { to, from, signal });
+  socket.on("signal", function ({ to, from, signal }) {
+    socket.to(to).emit("signal", { to, from, signal });
   });
 
   // listen for disconnects
-  socket.on('disconnect', function() {
-    namespace.emit('disconnected peer', socket.id);
-  })
-
+  socket.on("disconnect", function () {
+    namespace.emit("disconnected peer", socket.id);
+  });
 });
 
 module.exports = { app };
