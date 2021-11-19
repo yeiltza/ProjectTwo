ae1c367b393c27e88a117f6c1674e68a3b150631
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2021-11-18 23:27:20 -0600

    Moving button redirects to JS (#20)
    * npm install
    
    * Working on buttons to redirect to room
    
    * fixing buttons
    
    * moving button redirect functionality to javascript
    
    Co-authored-by: Jeebe Diop <jeebe.diop@gmail.com>

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,14 @@
 "use strict";
 
+document.getElementById("t1").addEventListener("click", changeLocation);
+document.getElementById("t2").addEventListener("click", changeLocation);
+document.getElementById("t3").addEventListener("click", changeLocation);
+document.getElementById("t4").addEventListener("click", changeLocation);
+
+function changeLocation(){
+  window.location.href = "../room.html";
+}
+
 // DOM Stuff
 const logo = document.querySelector(".logo");
 logo.addEventListener("click", goHome);
